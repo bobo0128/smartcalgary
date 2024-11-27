6737df2ad0bf673a1e030b0e8e0c788e027b830e
Yingbo <liuyingbo81@hotmail.com>
2024-11-27 08:48:01 -0700

    move backendURL to .env file

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,16 +1,24 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import dotenv from 'dotenv';
+dotenv.config();
+
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:4000', // Replace with your backend URL
-        changeOrigin: true,
-        secure: false,
+export default defineConfig(() =>{
+  const backendUrl = process.env.VITE_BACKEND_URL;
+
+  return {
+    plugins: [react()],
+    server: {
+      proxy: {
+        '/api': {
+          target: backendUrl, // Replace with your backend URL
+          changeOrigin: true,
+          secure: false,
+        },
       },
     },
-  },
+  }
+
 })
